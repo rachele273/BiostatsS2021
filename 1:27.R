b7181a65a8a8dc2896088458c5c710b80ae30e71
rachele273 <edidi003@umn.edu>
2021-01-27 13:59:14 -0600

    oops this is the rest of the code from 1/27

--- 1:27.R
+++ 1:27.R
@@ -4,3 +4,26 @@
 
 library (tidyverse)
 2-5 #how to subtract
+1!=2
+1>2
+#^exploring logic statements 
+
+#vectors
+c(1,2,3) #numeric
+c(TRUE, FALSE, TRUE) #logical
+c("Rachel", "Edidin") #character...remember case sensitivity
+
+#variable
+student_1<- c("Rachel", "Edidin")
+a<- c(1, 3, 4)
+5+a
+5*a
+a>3
+b<- c(2, 5, 1)
+a+b
+a*b
+length(student_1)
+c(4)*c(1, 2, 3, 4, 5)
+16*4
+32*4
+c(4)*c(2, 4, 8, 16, 32)
