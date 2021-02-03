2fb72ca5d22156c8b4541afd934ac10019cc498f
rachele273 <edidi003@umn.edu>
2021-02-03 17:52:51 -0600

    february 3

--- 2:3.R
+++ 2:3.R
@@ -11,3 +11,14 @@ ggplot(data=iris, aes(x=Sepal.Length))+
   geom_histogram(bins=30)
 ggplot(data=iris, aes(x=Sepal.Length))+
   geom_density(adjust=.3, color="blue", fill="pink")
+ggplot(iris, aes(x=Sepal.Length, fill=Species))+
+  geom_histogram(bins=20)+
+  facet_wrap(~Species, ncol=1)
+ggplot(iris, aes(x=Sepal.Length, fill=Species))+
+  geom_density(alpha=.4)
+ggplot(iris, aes(x=Sepal.Length, fill=Species))+
+  geom_histogram(bins=20)
+ggplot(iris, aes(x=Species, y=Sepal.Length))+
+  geom_point()
+ggplot(iris, aes(x=Species, y=Sepal.Length))+
+  geom_jitter(set_height=0)
